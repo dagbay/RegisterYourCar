87dcb141edd0017dcde794f1133da3b222803a39
dagbay <80764810+dagbay@users.noreply.github.com>
2021-09-05 17:35:47 +1000

    Updated backend and frontend of the website
    Next iteration, I will be working towards filling up the database with test information so that I can further improve on the website.

--- website_car/views.py
+++ website_car/views.py
@@ -1,16 +1,19 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
 # from .models import Note
 # from . import db
-import json
+# import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
-def home():
-    return render_template("home.html")
-
 @views.route('/contact_us')
 def contact_us():
-    return render_template("contact_us.html")
+    return render_template("contact_us.html", user=current_user)
+
+@views.route('/')
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
+
+
 
